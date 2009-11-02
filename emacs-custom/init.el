7af70e31940331e9c6219ee2dadb0dff047e7a29
Derek Williams <derek@nebvin.ca>
2009-11-01 18:34:31 -0700

    Fix scala-mode loading

--- emacs-custom/init.el
+++ emacs-custom/init.el
@@ -1,10 +1,14 @@
-(add-to-list 'load-path (file-name-directory
-                         (or (buffer-file-name) load-file-name))) 
+(setq custom-dir (file-name-directory
+                  (or (buffer-file-name) load-file-name)))
+(add-to-list 'load-path custom-dir)
+(add-to-list 'load-path (concat custom-dir "scala-mode"))
 
 (load "themes/tango")
 (color-theme-tango)
 
 (load "defuns")
 (load "bindings")
-(load "scala-mode")
 (load "sbt")
+
+(require 'scala-mode-auto)
+
--- emacs-custom/scala-mode.el
+++
@@ -1,6 +0,0 @@
-;; Use spaces instead of tabs
-(defun me-turn-off-indent-tabs-mode ()
-  (setq indent-tabs-mode nil))
-(add-hook 'scala-mode-hook 'me-turn-off-indent-tabs-mode)
-
-(load "scala-mode/scala-mode-auto")
