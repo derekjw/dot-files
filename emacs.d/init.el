e1b041193b7f8fd63894ee1ab228ae4e5352c496
Derek Williams <derek@nebvin.ca>
2010-05-20 00:02:49 -0600

    Add org-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,3 +8,15 @@
 
 (autoload 'my-site-start (concat dotfiles-dir "my-site-start") nil t)
 (my-site-start (concat dotfiles-dir "/site-start.d/"))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Documents/Orgdev.org"))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
